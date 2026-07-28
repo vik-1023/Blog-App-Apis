749902234b67129b522ebc592ba7b2563ad243c3
vicky <vkaditya878@gmail.com>
2026-07-28 22:23:08 +0530

    refactor: extract pagination defaults into AppConstants

---
+++ src/main/java/org/blog/apis/utils/AppConstants.java
@@ -0,0 +1,12 @@
+package org.blog.apis.utils;
+
+public class AppConstants {
+    private AppConstants() {
+
+    }
+
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "5";
+    public static final String SORT_BY = "postId";
+    public static final String SORT_DIR = "asc";
+}
